8bce7ea69e968560be4401582c9f68d533405d2d
Rishabh <rishabhbhatt956@gmail.com>
2025-06-07 15:56:21 +0530

    Graphql intro (PENDING)

---
+++ Graphql/Graphql Crash Course/server/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+        type User{
+            id: ID!
+            name: String!
+            username: String!
+            email: String!
+        }
+        type Todo{
+            id: ID! 
+            title: String!
+            completed: Boolean
+        }
+
+        type Query{
+            getTodos: [Todo]
+            getAllUsers: [User]
+            getUser(id: ID!): User
+        }
+    `,
+    resolvers: {
+      Query: {
+        getTodos: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+        getAllUsers: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+        getUser: async (parent, {id}) =>
+          (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data,
+      },
+    },
+  });
+
+  await server.start();
+  app.use(express.json());
+  app.use(cors());
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(8000, () => console.log("Server is listening at PORT 8000"));
+}
+startServer();
